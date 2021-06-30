af99fa56a08cf0fc53254e3b187d1ceb602c3083
Yogeshtalreja <yogeshtalreja54@gmail.com>
2021-06-30 15:04:54 +0500

    Data Fetching

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Weather from './components/Weather';
+import { useEffect } from 'react';
 
 function App() {
+  const APIKEY = "d6fa07f6c5abb09eb9064698144d78bb";
+
+
+    fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${APIKEY}`)
+    .then((res)=>res.json())
+    .then((result)=> console.log(result));
+  
+
   return (
     <div className="App">
  <Weather />
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.css';
 const Weather = ()=>{
     return(
 <div>
@@ -7,9 +8,17 @@ const Weather = ()=>{
 
 <div className="cards">
     <h1>Weather Check</h1>
-    <div className="py-5">
+    <h5 className="py-5">
+        <i className="wi wi-day-sunny display-1"></i>
 
 
+    </h5>
+    <h1 className="py-2">
+        25&deg;
+    </h1>
+    {minMax(23,56)}
+    <div className="py-2">
+        Slow Rain
     </div>
 </div>
 
@@ -21,6 +30,19 @@ const Weather = ()=>{
     );
 
 
+}
+
+const minMax = (min,max) =>{
+    return(
+<h3>
+    <span className="px-4">
+        Min = {min}&deg; 
+    </span>
+    <span className="px-4">
+        Max = {max}&deg; 
+    </span>
+</h3>
+    );
 }
 
 export default Weather;
