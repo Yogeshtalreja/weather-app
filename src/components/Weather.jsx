96229a921dcf23cb2ef98708816edc34d898bd95
Yogeshtalreja <yogeshtalreja54@gmail.com>
2021-06-30 14:22:12 +0500

    Initial Commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const Weather = ()=>{
+    return(
+<div>
+<div className="container">
+
+<div className="cards">
+    <h1>London</h1>
+    <div className="py-5">
+
+        
+    </div>
+</div>
+
+</div>
+
+</div>
+
+
+    );
+
+
+}
+
+export default Weather;
