86eae5fcd33ea6a9046e1b42ff741ef08515cce5
Yogeshtalreja <yogeshtalreja54@gmail.com>
2021-07-03 16:29:13 +0500

    Weather Update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
 import Weather from './components/Weather';
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
+import {Form , Button} from 'react-bootstrap';
 function App() {
-  const APIKEY = "d6fa07f6c5abb09eb9064698144d78bb";
-
-
-    fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${APIKEY}`)
+  const APIKEY = 'de66fbb1ed3365202fef510725ba0786';
+  const [data,setData] = useState({});
+ const [state , setState] = useState({
+   city : '',
+   Country : ''
+ });
+   const onClick = () =>{
+    console.log(state);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${state.city},${state.Country}&appid=${APIKEY}`)
     .then((res)=>res.json())
-    .then((result)=> console.log(result));
+    .then((total)=>{
+     setData(total);
+     console.log(total);
+     
+     }
+    
+    );
   
+  }
+
+  const handleChange = ((e)=>{
+      console.log(e.target.name); 
+      
+    if(e.target.name === 'city'){
+        
+          setState({...state,city: e.target.value})
+          console.log(state);
+      }
+      if(e.target.name === 'country'){
+        setState({...state,Country:e.target.value});
+        console.log(state);
+      }
+  })
+
+  console.log(data)
 
   return (
     <div className="App">
- <Weather />
+      <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>City Name</Form.Label>
+    <Form.Control type="text" placeholder="Enter City" value={state.city} name="city" onChange={handleChange} />
+    <Form.Text className="text-muted">
+      Please Enter City Name 
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Country Name</Form.Label>
+    <Form.Control type="text" placeholder="Enter Country" name="country" value={state.Country} onChange={handleChange}/>
+    <Form.Text className="text-muted">
+      Please Enter Country Name 
+    </Form.Text>
+  </Form.Group>
+<Button variant="primary" onClick={onClick}>
+    Submit
+  </Button>
+</Form>
+      <Weather data={data}/>
     </div>
   );
 }
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
-const Weather = ()=>{
+const Weather = (props)=>{
     return(
 <div>
 <div className="container">
